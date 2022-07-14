ff1e50d474cf389c5c4116a08f2cc03867e3a0b7
Marcio Yokana <cadastroykn@gmail.com>
2022-07-14 00:09:40 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import NotificationBotton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá Amapá</h1>
-      <NotificationBotton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+        </div>
+      </section>
+    </main>
     </>
   )
 }
